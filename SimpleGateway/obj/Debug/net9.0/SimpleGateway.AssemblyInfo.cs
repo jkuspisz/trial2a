99b9b66e1e40daeaec66e5db9471411bdba09914
James Kuspisz <jkuspisz@gmail.com>
2025-07-26 11:05:19 +0100

    Fix UseNpgsql extension method by ensuring proper Npgsql package installation

--- SimpleGateway/obj/Debug/net9.0/SimpleGateway.AssemblyInfo.cs
+++ SimpleGateway/obj/Debug/net9.0/SimpleGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94865a36fb6b0b73f6cbc5c54c4394cdb53ae720")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6a7ec35b257605fe7f91ff689199c4dc90d64f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
