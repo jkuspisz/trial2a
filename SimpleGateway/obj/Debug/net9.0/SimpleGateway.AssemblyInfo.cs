10b3f1bc20bc904638152d36ba286a77ecda085c
James Kuspisz <jkuspisz@gmail.com>
2025-07-27 15:55:44 +0100

    Implement Previous Experience functionality following TestPractice pattern - fixed sidebar, controller methods, and database integration

--- SimpleGateway/obj/Debug/net9.0/SimpleGateway.AssemblyInfo.cs
+++ SimpleGateway/obj/Debug/net9.0/SimpleGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97481269d80129fc5b483a5663d9913e4c452d21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a73437113ab92667bcce17df3eea33224e8c2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
