39665819b517ca25a7ed466fb4fe38bcc4c0c5d4
James Kuspisz <jkuspisz@gmail.com>
2025-07-27 13:10:32 +0100

    Update uploads functionality and file handling

--- SimpleGateway/obj/Debug/net9.0/SimpleGateway.AssemblyInfo.cs
+++ SimpleGateway/obj/Debug/net9.0/SimpleGateway.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SimpleGateway")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0021c4b47eb6808d5162037b16270a1805b17898")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35a687665970ea73452324e10abe280c976606b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("SimpleGateway")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SimpleGateway")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
